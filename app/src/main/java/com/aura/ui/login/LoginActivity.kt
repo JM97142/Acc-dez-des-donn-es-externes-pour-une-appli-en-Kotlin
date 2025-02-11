0743adbd86135994901107880d1fdf881c3b80a0
JM97142 <jeanmichel.taret@gmail.com>
2025-02-11 12:49:52 +0100

    update LoginViewModel

--- app/src/main/java/com/aura/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aura/ui/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.aura.ui.login
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -29,14 +30,8 @@ class LoginActivity : AppCompatActivity() {
     observeForm()
     observeLoading()
     formState()
-
-    binding.login.setOnClickListener {
-
-      val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-      startActivity(intent)
-
-      finish()
-    }
+    buttonLogin()
+    observeAccess()
   }
 
   private fun observeForm() {
@@ -58,5 +53,43 @@ class LoginActivity : AppCompatActivity() {
   private fun formState() {
     val id = binding.identifier.text.toString()
     val password = binding.password.text.toString()
+
+    viewModel.verifyForm(id, password)
+  }
+
+  private fun userLogin(id: String, password: String) {
+    lifecycleScope.launch {
+      try {
+          viewModel.login(id, password)
+      }
+      catch (e: Exception) {
+        Toast.makeText(this@LoginActivity, "Fail to login: ${e.message}", Toast.LENGTH_LONG).show()
+      }
+    }
+  }
+
+  private fun buttonLogin() {
+    val id = binding.identifier.text.toString()
+    val password = binding.password.text.toString()
+
+    userLogin(id, password)
+  }
+
+  private fun accessGranted(id: String) {
+    val intent = Intent(this, HomeActivity::class.java)
+    intent.putExtra("user", id)
+    startActivity(intent)
+
+    finish()
+  }
+
+  private fun observeAccess() {
+    lifecycleScope.launch {
+      viewModel.isAccessGranted.collect { id ->
+        id?.let {
+          accessGranted(it)
+        }
+      }
+    }
   }
 }
--- app/src/main/java/com/aura/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/aura/ui/login/LoginViewModel.kt
@@ -1,19 +1,55 @@
 package com.aura.ui.login
 
 import androidx.lifecycle.ViewModel
+import com.aura.data.api.NetworkModule
+import com.aura.data.model.login.LoginModelRequest
+import com.aura.data.api.ApiService
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-
+import kotlinx.coroutines.withContext
 
 class LoginViewModel : ViewModel() {
 
+    // StateFlow to enable or disable login button
     private val _isFormCorrect = MutableStateFlow(false)
     val isFormCorrect: StateFlow<Boolean> get() = _isFormCorrect
 
+    // StateFlow manage loading state
     private val _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> get() = _isLoading
 
-    fun verifyForm(id: String, password: String) {
-        _isFormCorrect.value = id.isNotEmpty() && password.isNotEmpty()
+    //StateFlow to notify if login is successful
+    private val _isAccessGranted = MutableStateFlow<String?>(null)
+    val isAccessGranted: StateFlow<String?> get() = _isAccessGranted
+
+    // Verify form state
+    fun verifyForm(identifier: String, password: String) {
+        _isFormCorrect.value = identifier.isNotEmpty() && password.isNotEmpty()
+    }
+
+    // API Call
+    suspend fun login(id: String, password: String) {
+        _isLoading.value = true
+        try {
+            val loginRequest = LoginModelRequest(id, password)
+            val networkModule = NetworkModule.provideRetrofit().create(ApiService::class.java)
+            val result = withContext(Dispatchers.IO) {
+                networkModule.login(loginRequest)
+            }
+
+            _isLoading.value = true
+
+            if (result.accessGranted) {
+                _isAccessGranted.value = id
+            }
+            else {
+                throw Exception("Fail to login: $result")
+            }
+        }
+        catch (e: Exception) {
+            _isLoading.value = false
+            throw e
+        }
     }
 }
